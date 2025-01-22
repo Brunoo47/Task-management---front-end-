3fb073a5d95e5687abf5f9ca6e0f1d43e98b4c16
Brunoo47 <brunoostroskiprogramador@gmail.com>
2025-01-21 21:53:23 -0300

    add pages

--- app-front/src/app/app.component.ts
+++ app-front/src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+
+interface Tarefa {
+  id: number;
+  nome: string;
+  concluida: boolean;
+}
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-lista-tarefas',
+  templateUrl: './lista-tarefas.component.html',
+  styleUrls: ['./lista-tarefas.component.css']
 })
-export class AppComponent {
-  title = 'app-front';
+export class ListaTarefasComponent implements OnInit {
+  tarefas: Tarefa[] = [
+    { id: 1, nome: 'Estudar Angular', concluida: false },
+    { id: 2, nome: 'Fazer compras', concluida: false },
+    { id: 3, nome: 'Lavar roupa', concluida: true },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleConcluida(id: number): void {
+    const tarefa = this.tarefas.find(t => t.id === id);
+    if (tarefa) {
+      tarefa.concluida = !tarefa.concluida;
+    }
+  }
+
+  excluirTarefa(id: number): void {
+    this.tarefas = this.tarefas.filter(tarefa => tarefa.id !== id);
+  }
 }
